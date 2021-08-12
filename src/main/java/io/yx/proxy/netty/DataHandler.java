40b5282fbc198df63ca9617afd947036a39c3e71
YX <503203440@qq.com>
2021-08-12 15:43:48 +0800

    移除抛出异常，不输出多余的信息

--- src/main/java/io/yx/proxy/netty/DataHandler.java
+++ src/main/java/io/yx/proxy/netty/DataHandler.java
@@ -7,8 +7,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-import java.nio.charset.StandardCharsets;
-
 /**
  * 数据处理程序
  */
@@ -23,27 +21,31 @@ public class DataHandler extends ChannelInboundHandlerAdapter {
     }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelActive(ChannelHandlerContext ctx) {
+        log.info("[channelActive] upstreamChannel:{} downstreamChannel:{}", upstreamChannel.remoteAddress(), ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf byteBuf = (ByteBuf) msg;
-        log.info("GET DATA:{}", byteBuf.toString(StandardCharsets.UTF_8));
         byteBuf.retain();
         upstreamChannel.writeAndFlush(byteBuf);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         upstreamChannel.close();
         ctx.close();
-        log.error("连接异常", cause);
+        log.error("[exceptionCaught]", cause);
     }
 
     /**
      * 当从 ChannelPipeline 中移除 ChannelHandler 时被调用
      */
     @Override
-    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+    public void handlerRemoved(ChannelHandlerContext ctx) {
         upstreamChannel.close();
-        log.info("handlerRemoved关闭下游管道");
         ctx.close();
+        log.info("[handlerRemoved] upstreamChannel:{} downstreamChannel:{}", upstreamChannel.remoteAddress(), ctx.channel().remoteAddress());
     }
 }
